308b3ee80ab80002ab207abcb093fc22c30954ca
szkrkr <SuzukiRikuro@suzukirokurou-no-MacBook-Pro.local>
2016-05-15 06:12:09 +0900

    一旦GetUserID, GetHostIDメソッド実装しました。

--- SpaApp/app/src/main/java/togepi/spaapp/SQLite/SQLite.java
+++ SpaApp/app/src/main/java/togepi/spaapp/SQLite/SQLite.java
@@ -1,7 +1,9 @@
 package togepi.spaapp.SQLite;
 
-import java.util.HashMap;
-import java.util.Map;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 import togepi.spaapp.utility.Genre;
 import togepi.spaapp.utility.PartySetting;
@@ -11,44 +13,104 @@ import togepi.spaapp.utility.PartySetting;
  */
 public class SQLite {
 
+
     /**
      * パーティIDをDBから取得する
      * @return
      */
-    public String GetHostID(){
-        return "";
+    public String GetHostID(Context context){
+        DatabaseOpenHelper databaseOpenHelper = new DatabaseOpenHelper(context);
+        SQLiteDatabase db = databaseOpenHelper.getReadableDatabase();
+
+        Cursor c = db.query("partySettings", new String[] { "hostID" }, null, null, null, null, null);
+
+        boolean mov = c.moveToFirst();
+        String hostId = null;
+        hostId = c.getString(0);
+
+//        while (mov) {
+//            hostId = c.getString(0);
+//            mov = c.moveToNext();
+//        }
+        return hostId;
     }
 
     /**
      * パーティIDをDBに登録する
      * @param hostID
      */
-    public void SetHostID(String hostID){
-
+    public void SetHostID(String hostID,Context context){
+        final DatabaseOpenHelper databaseOpenHelper = new DatabaseOpenHelper(context);
+        SQLiteDatabase db = null;
+        try {
+            db = databaseOpenHelper.getWritableDatabase();
+            ContentValues insertValues = new ContentValues();
+            insertValues.put("hostID", hostID);
+            db.insert("partySettings", null, insertValues);
+        } finally {
+            if(db != null){
+                db.close();
+            }
+        }
     }
 
     /**
      * UserIDをDBから取得する
      * @return
      */
-    public String GetUserID(){
-        return "";
+    public String GetUserID(Context context){
+        DatabaseOpenHelper databaseOpenHelper = new DatabaseOpenHelper(context);
+        SQLiteDatabase db = databaseOpenHelper.getReadableDatabase();
+
+        Cursor c = db.query("users", new String[] { "_id" }, null, null, null, null, null);
+
+        boolean mov = c.moveToFirst();
+        String userId = null;
+        userId = c.getString(0);
+
+//        while (mov) {
+//            userId = c.getString(0);
+//            mov = c.moveToNext();
+//        }
+        return userId;
     }
 
     /**
      * UserIDをDBに登録する
      * @param userID
      */
-    public void SetUserID(String userID){
-
+    public void SetUserID(String userID,Context context){
+        final DatabaseOpenHelper databaseOpenHelper = new DatabaseOpenHelper(context);
+        SQLiteDatabase db = null;
+        try {
+            db = databaseOpenHelper.getWritableDatabase();
+            ContentValues insertValues = new ContentValues();
+            insertValues.put("_id", userID);
+            db.insert("users", null, insertValues);
+        } finally {
+            if(db != null){
+                db.close();
+            }
+        }
     }
 
     /**
      * パーティ設定をDBから取得する
      * @return
      */
-    public PartySetting GetPartySetting(){
+    public PartySetting GetPartySetting(Context context){
         PartySetting partySetting = new PartySetting();
+//
+//        DatabaseOpenHelper databaseOpenHelper = new DatabaseOpenHelper(context);
+//        SQLiteDatabase db = databaseOpenHelper.getReadableDatabase();
+//
+//        Cursor c = db.query("partySettings", new String[] { "hostID", "initialMoney", "prizeNumber", "RankingFlag"}, null, null, null, null, null);
+//
+//        boolean mov = c.moveToFirst();
+//        String hostId = c.getString(0);
+//        while (mov) {
+//            mov = c.moveToNext();
+//        }
 
         return partySetting;
     }
@@ -56,7 +118,7 @@ public class SQLite {
     /**
      * パーティ設定をDBに登録する
      */
-    public void SetPartySetting(PartySetting partySetting){
+    public void SetPartySetting(PartySetting partySetting,Context context){
 
     }
 
