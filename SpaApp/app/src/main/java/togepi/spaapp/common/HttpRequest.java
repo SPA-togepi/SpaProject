6c2b8406c9971142f5ea1992e851d477492a1f3c
szkrkr <SuzukiRikuro@suzukirokurou-no-MacBook-Pro.local>
2016-05-15 05:15:33 +0900

    http req, res修正

--- SpaApp/app/src/main/java/togepi/spaapp/common/HttpRequest.java
+++ SpaApp/app/src/main/java/togepi/spaapp/common/HttpRequest.java
@@ -3,29 +3,50 @@ package togepi.spaapp.common;
 import android.util.Log;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
-
+/*
+ * HTTPリクエスト操作クラス。<br />
+ */
 public class HttpRequest {
-
-    public HttpRequest(){}
-
     /*
-    * POST JSONの送信
-    * @param urlStr
-    * @param jsonStr
-    * */
-    public void httpRequest(String urlStr, String jsonStr){
+     * GETリクエスト。<br />
+     * @param uri URI
+     */
+//    protected HttpResponse doGet(String uri) throws IOException {
+//        URL urlObj = new URL("http://" + this.webHost + uri);
+//        HttpURLConnection http = (HttpURLConnection) urlObj.openConnection();
+//        http.setRequestMethod("GET");
+//        http.connect();
+//        // 結果を取得
+//        return getResponse(http, null);
+//    }
+    /*
+     * POSTリクエスト。<br />
+     * @param uri URI
+     * @param postData POSTするデータ
+     */
+    protected HttpResponse doPost(String urlStr, String jsonStr) throws IOException {
+
+        HttpResponse httpResponse = null;
         try {
             String buffer = "";
             HttpURLConnection con = null;
@@ -46,10 +67,8 @@ public class HttpRequest {
             buffer = reader.readLine();
 
             JSONArray jsonArray = new JSONArray(buffer);
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                Log.d("HTTP REQ", jsonObject.getString("name"));
-            }
+
+            httpResponse = getResponse(con, jsonArray);
             con.disconnect();
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -60,5 +79,76 @@ public class HttpRequest {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        return httpResponse;
     }
+    /*
+     * レスポンスデータを取得する。<br />
+     * @param http http接続オブジェクト
+     * @param webEncode エンコーディング
+     */
+    private HttpResponse getResponse(HttpURLConnection http, JSONArray jsonArray) throws IOException {
+        HttpResponse response = new HttpResponse();
+        // ステータスコードの取得
+        response.setStatus(http.getResponseCode());
+
+        HttpURLConnection con = null;
+        URL url = null;
+        String urlStr = "http://ec2-54-199-215-9.ap-northeast-1.compute.amazonaws.com:3000/";
+
+        try {
+            // URLの作成
+            url = new URL(urlStr);
+            // 接続用HttpURLConnectionオブジェクト作成
+            con = (HttpURLConnection) url.openConnection();
+            // リクエストメソッドの設定
+            con.setRequestMethod("POST");
+            // リダイレクトを自動で許可しない設定
+            con.setInstanceFollowRedirects(false);
+            // URL接続からデータを読み取る場合はtrue
+            con.setDoInput(true);
+            // URL接続にデータを書き込む場合はtrue
+            con.setDoOutput(true);
+
+            // 接続開始
+            con.connect();
+
+            // 本文の取得
+            InputStream in = con.getInputStream();
+            byte bodyByte[] = new byte[1024];
+            in.read(bodyByte);
+            //Jsonを取得する
+            String readSt = readInputStream(in);
+            in.close();
+
+            response.setBody(bodyByte.toString());
+            response.setJSONArray(jsonArray);
+
+            return response;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public String readInputStream(InputStream in) throws IOException, UnsupportedEncodingException {
+        StringBuffer sb = new StringBuffer();
+        String st = "";
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        while ((st = br.readLine()) != null) {
+            sb.append(st);
+        }
+        try {
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return sb.toString();
+    }
+
 }
--- SpaApp/app/src/main/java/togepi/spaapp/common/HttpResponse.java
+++ SpaApp/app/src/main/java/togepi/spaapp/common/HttpResponse.java
@@ -1,94 +1,113 @@
 package togepi.spaapp.common;
 
-import android.os.AsyncTask;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import org.json.JSONArray;
 
 /**
  * Created by SuzukiRikuro on 2016/05/15.
  */
-public class HttpResponse extends AsyncTask<Void, Void, JSONObject> {
 
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
-        // doInBackground前処理
+public class HttpResponse {
+
+    private int status;
+    private String body;
+    private JSONArray jsonArray;
+
+
+    public void setStatus(int status) {
+        this.status = status;
     }
 
-    @Override
-    protected JSONObject doInBackground(Void... params) {
-        HttpURLConnection con = null;
-        URL url = null;
-        String urlStr = "http://ec2-54-199-215-9.ap-northeast-1.compute.amazonaws.com:3000/";
-
-        try {
-            // URLの作成
-            url = new URL(urlStr);
-            // 接続用HttpURLConnectionオブジェクト作成
-            con = (HttpURLConnection) url.openConnection();
-            // リクエストメソッドの設定
-            con.setRequestMethod("POST");
-            // リダイレクトを自動で許可しない設定
-            con.setInstanceFollowRedirects(false);
-            // URL接続からデータを読み取る場合はtrue
-            con.setDoInput(true);
-            // URL接続にデータを書き込む場合はtrue
-            con.setDoOutput(true);
-
-            // 接続開始
-            con.connect();
-
-            // 本文の取得
-            InputStream in = con.getInputStream();
-            byte bodyByte[] = new byte[1024];
-            in.read(bodyByte);
-            //Jsonを取得する
-            String readSt = readInputStream(in);
-            in.close();
-
-            return new JSONObject(readSt);
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        return null;
+    public void setBody(String body) {
+        this.body = body;
     }
 
-    @Override
-    protected void onPostExecute(JSONObject result) {
-        super.onPostExecute(result);
-        // doInBackground後処理
-    }
-
-    public String readInputStream(InputStream in) throws IOException, UnsupportedEncodingException {
-        StringBuffer sb = new StringBuffer();
-        String st = "";
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-        while ((st = br.readLine()) != null) {
-            sb.append(st);
-        }
-        try {
-            in.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return sb.toString();
+    public void setJSONArray(JSONArray JSONArray) {
+        this.jsonArray = JSONArray;
     }
 }
+//public class HttpResponse extends AsyncTask<Void, Void, JSONObject> {
+//
+//    private LinkedHashMap<String, String> header;
+//    private int status;
+//
+//    @Override
+//    protected void onPreExecute() {
+//        super.onPreExecute();
+//        // doInBackground前処理
+//    }
+//
+//    @Override
+//    protected JSONObject doInBackground(Void... params) {
+//        HttpURLConnection con = null;
+//        URL url = null;
+//        String urlStr = "http://ec2-54-199-215-9.ap-northeast-1.compute.amazonaws.com:3000/";
+//
+//        try {
+//            // URLの作成
+//            url = new URL(urlStr);
+//            // 接続用HttpURLConnectionオブジェクト作成
+//            con = (HttpURLConnection) url.openConnection();
+//            // リクエストメソッドの設定
+//            con.setRequestMethod("POST");
+//            // リダイレクトを自動で許可しない設定
+//            con.setInstanceFollowRedirects(false);
+//            // URL接続からデータを読み取る場合はtrue
+//            con.setDoInput(true);
+//            // URL接続にデータを書き込む場合はtrue
+//            con.setDoOutput(true);
+//
+//            // 接続開始
+//            con.connect();
+//
+//            // 本文の取得
+//            InputStream in = con.getInputStream();
+//            byte bodyByte[] = new byte[1024];
+//            in.read(bodyByte);
+//            //Jsonを取得する
+//            String readSt = readInputStream(in);
+//            in.close();
+//
+//            return new JSONObject(readSt);
+//
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return null;
+//    }
+//
+//    @Override
+//    protected void onPostExecute(JSONObject result) {
+//        super.onPostExecute(result);
+//        // doInBackground後処理
+//    }
+//
+//    public String readInputStream(InputStream in) throws IOException, UnsupportedEncodingException {
+//        StringBuffer sb = new StringBuffer();
+//        String st = "";
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+//        while ((st = br.readLine()) != null) {
+//            sb.append(st);
+//        }
+//        try {
+//            in.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        return sb.toString();
+//    }
+//
+//    public void setStatus(int status) {
+//        this.status = status;
+//    }
+//
+//    public void setHeader(LinkedHashMap<String,String> header) {
+//        this.header = header;
+//    }
+//}
